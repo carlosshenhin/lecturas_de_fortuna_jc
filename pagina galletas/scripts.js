e70d0fdeb4c624303abbc131ef7398f3abe07552
JustIsJJ <zerojosevf@gmail.com>
2023-09-15 14:55:39 -0400

    Texto en el JS

--- pagina galletas/scripts.js
+++ pagina galletas/scripts.js
@@ -28,7 +28,44 @@ function ListRandom(list) {
     return list;
 }
 
+var arrayTxt1 = [
+    "Sí tienes problemas en casa,", 
+    "Sí necesitas ayuda con tus pensamientos pesados,", 
+    "Sí piensas que esta mal sentirse mal,", 
+    "Sí no quieres hablar con otra persona sobre tus sentimientos,", 
+    "Sí te sientes solo,", 
+    "Sí no quieres ser un 'problema',", 
+    "Sí te sientes confundido,", 
+    "Sí quieres llorar,", 
+    "Sí no tienes una enorme cantidad de amigos,", 
+    "Sí sientes que otros te juzgan en secreto,"
+]
+var arrayTxt2 = [
+    "eso significa que eres una persona normal, no te preocupes por eso,",
+    "eso significa que tal vez no estas en tus mejores dias, no te sientas mal solo por eso,",
+    "eso significa que necesitas un poco de ayuda, eso es normal para cualquiera,",
+    "eso significa que tienes un poco de trabajo personal, tomate tu tiempo,",
+    "eso significa que necesitas respirar y descansar, tomate tu tiempo,",
+    "eso significa que no debes dejar de tener esperanza, no te rindas,",
+    "eso significa que debes pensar en tu futuro, no te agobies,",
+    "eso significa que necesitas liberar ese peso, no te sobrecargues,",
+    "eso significa que debes buscar un poco de ayuda, esta bien no estar bien,",
+    "eso significa que pensar quiza por un momento antes de actuar, piensa las cosas bien,"
+]
+var arrayTxt3 = [
+    "deberias intentar hacer algo al respecto.",
+    "deberias pensar en quien pueda ayudarte, esta bien necesitar ayuda.",
+    "deberias calmarte y pensar en paz.",
+    "deberias ayudarte un poco buscando ayuda.",
+    "deberias dejar un poco el orgullo.",
+    "deberias medir un poco tus acciones, ten cuidado.",
+    "deberias dejar de reprimirte una y otra vez.",
+    "deberias darte un tiempo de reposo solo para ti.",
+    "deberias hablar sobre lo que te pasa, esta bien.",
+    "deberias ser tú, y no sentirte mal por eso.",
+]
 
+console.log(arrayTxt1[7])
 
 
 
